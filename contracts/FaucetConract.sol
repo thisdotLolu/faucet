0c659f3d62f0ae2c49c6fb405b081a61dbb2b7e9
Lolu <davidebe63@gmail.com>
2023-02-01 19:41:29 +0100

    memory

--- contracts/FaucetConract.sol
+++ contracts/FaucetConract.sol
@@ -1,7 +1,30 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+
+
 contract Faucet{
+
+    address[] public funders; 
+
     receive() external payable {}
-    
+
+    function addFunds() external payable{
+        funders.push(msg.sender);
+    }
+
+    function justTesting() external pure returns(uint256){
+        return 2 + 2;
+    }
+
+    function getAllFunders() public view returns (address[] memory){
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 _index) external view returns(address){
+        address[] memory _funders = this.getAllFunders();
+        return _funders[_index];
+    }
+
 }
+
