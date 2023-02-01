0b03b7b8a7791abcfeb26495645de0d2bb2b96e6
Lolu <davidebe63@gmail.com>
2023-02-01 19:41:29 +0100

    memory

---
+++ contracts/Test.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Test{
+    function test(uint testNum) external pure returns (uint){
+        
+        assembly {
+            let _num := 4
+            let _fmp := mload(0x40)
+        }
+        
+        uint8[3] memory items =[1,2,3];
+        return testNum;
+    }
+}
